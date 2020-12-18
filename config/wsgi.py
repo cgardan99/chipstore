992f1962329ff912038082b62c64d9f578476c7e
Carlos Daniel <carlosdany58@hotmail.com>
2020-12-17 20:42:52 -0600

    cambio en el wsgi para leer las variables de entorno

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,13 +1,21 @@
 """
-WSGI config for radon project.
+WSGI config for Chipstore project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
 """
-
-import os, environ, sys
+import os
+import sys
+import environ
 from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
