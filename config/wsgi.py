4aba9ad5f60ff7704e84f902e00f6de20f877666
Carlos Daniel <carlosdany58@hotmail.com>
2020-12-17 20:37:29 -0600

    cambio en el wsgi para leer las variables de entorno

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,38 +1,36 @@
 """
-WSGI config for Chipstore project.
+WSGI config for radon project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
+
 import os
-import sys
-from pathlib import Path
+import environ
 
 from django.core.wsgi import get_wsgi_application
 
-# This allows easy placement of apps within the interior
-# chipstore directory.
+env = environ.Env()
+READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
+WSGI_PATH = environ.Path(__file__)  # radon/radon/config/wsgi.py
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(ROOT_DIR / "chipstore"))
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+if READ_DOT_ENV_FILE:
+    # Operating System Environment variables have precedence over variables defined in the .env file,
+    # that is to say variables from the .env files will only be used if not defined
+    # as environment variables.
+    env_file = str(ROOT_DIR.path('.env'))
+    env.read_env(env_file)
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        env('DJANGO_SETTINGS_MODULE')
+    )
+else:
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "radon.config.settings.production"
+    )
+
 application = get_wsgi_application()
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
